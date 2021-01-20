19b75f843c82e77058705ec217398add06b1172e
Kalin Kostadinov <56891753+kalin-kostadinov@users.noreply.github.com>
2021-01-20 16:13:07 +0200

    Create DeepCopy.js
    function for object cloning

---
+++ DeepCopy.js
@@ -0,0 +1,9 @@
+function deepCopy(target) {
+    if (Array.isArray(target)) {
+        return target.map(deepCopy);
+    } else if (typeof target == 'object') {
+        return [...Object.entries(target)].reduce((a, [k,v]) => Object.assign(a, {[k]: deepCopy(v)}), {});
+    } else {
+        return target;
+    }
+}
